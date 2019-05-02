79152b22ed4f8edf085107a5f10c45076ba0df51
cesslab <anwarruff@gmail.com>
2019-05-02 14:46:57 -0400

    Added value treatment final outcome screen

--- exp/pages.py
+++ exp/pages.py
@@ -24,7 +24,7 @@ class FirstRoundEntry(Page):
         return {
             'round': self.round_number,
             'unopened': values,
-            'value_type': '%' if (self.session.config['probability_treatment']) else 'Points'
+            'value_type': '%' if (self.session.config['probability_treatment']) else ''
         }
 
     def buyout_min(self):
@@ -72,7 +72,7 @@ class RoundEntry(Page):
             'unopened': unopened_values,
             'opened': opened_values,
             'envelope': envelope,
-            'value_type': '%' if (self.session.config['probability_treatment']) else 'Points'
+            'value_type': '%' if (self.session.config['probability_treatment']) else ''
         }
 
     def buyout_min(self):
@@ -96,9 +96,77 @@ class RoundEntry(Page):
             self.player.participant.vars['buyout'] = self.player.buyout
 
 
-class Outcome(Page):
+class POutcome(Page):
     def is_displayed(self):
-        return self.round_number == Constants.num_rounds
+        return self.round_number == Constants.num_rounds and self.session.config['probability_treatment']
+
+    def vars_for_template(self):
+        offer = self.participant.vars['random_offer']
+        random_round = self.participant.vars['random_round']
+        random_number = self.participant.vars['random_number']
+        random_envelope_value = self.participant.vars['random_envelope_value']
+        unopened_values = self.participant.vars['unopened_values']
+        buyout = self.participant.vars['buyout']
+
+        accept_offer = buyout <= offer
+        won_prize = random_number <= random_envelope_value
+
+        if accept_offer:
+            payoff = c(offer).to_real_world_currency()
+        else:
+            # win prize
+            if won_prize:
+                payoff = c(self.session.config['prize'])
+            else:
+                payoff = c(0)
+        showup_fee = self.session.config['participation_fee']
+        final_payoff = payoff.to_real_world_currency(self.session) + showup_fee
+        return {
+            'random_round': random_round,
+            'unopened': unopened_values,
+            'envelope': random_envelope_value,
+            'prob_treatment': True,
+            'accept_offer': accept_offer,
+            'won_prize': won_prize,
+            'buyout': buyout,
+            'offer': offer,
+            'random_number': random_number,
+            'prize': c(self.session.config['prize']),
+            'payoff': payoff,
+            'showup_fee': showup_fee,
+            'final_payoff': final_payoff
+        }
+    
+    def before_next_page(self):
+        buyout = self.participant.vars['buyout']
+        offer = self.participant.vars['random_offer']
+
+        random_number = self.participant.vars['random_number']
+        random_envelope_value = self.participant.vars['random_envelope_value']
+
+        # buyout offer accepted
+        if buyout <= offer:
+            self.player.payoff = c(offer)
+        # buyout offer rejected
+        else:
+            # win prize
+            if random_number <= random_envelope_value:
+                self.player.payoff = c(self.session.config['prize'])
+            else:
+                self.player.payoff = 0
+
+        self.player.buyout = self.participant.vars['buyout']
+        self.player.random_offer = self.participant.vars['random_offer']
+        self.player.random_round = self.participant.vars['random_round']
+        self.player.random_envelope_value = self.participant.vars['random_envelope_value']
+        self.player.random_number = self.participant.vars['random_number']
+        self.player.won_prize = random_number <= random_envelope_value
+        self.player.offer_accepted = buyout <= offer
+
+
+class VOutcome(Page):
+    def is_displayed(self):
+        return self.round_number == Constants.num_rounds and not self.session.config['probability_treatment']
 
     def vars_for_template(self):
         offer = self.participant.vars['random_offer']
@@ -109,7 +177,16 @@ class Outcome(Page):
         buyout = self.participant.vars['buyout']
 
         accept_offer = buyout < offer
-        won_prize = random_number <= random_envelope_value
+        # buyout offer accepted
+        if buyout <= offer:
+            payoff = c(offer)
+        # buyout offer rejected
+        else:
+            payoff = c(random_envelope_value)
+
+        showup_fee = self.session.config['participation_fee']
+        final_payoff = payoff.to_real_world_currency(self.session) + showup_fee
+
 
         return {
             'random_round': random_round,
@@ -117,24 +194,38 @@ class Outcome(Page):
             'envelope': random_envelope_value,
             'prob_treatment': self.session.config['probability_treatment'],
             'accept_offer': accept_offer,
-            'won_prize': won_prize,
             'buyout': buyout,
             'offer': offer,
             'random_number': random_number,
-            'prize': self.session.config['prize'],
+            'payoff': payoff,
+            'showup_fee': showup_fee,
+            'final_payoff': final_payoff
         }
-    
+
     def before_next_page(self):
+        buyout = self.participant.vars['buyout']
+        offer = self.participant.vars['random_offer']
+        random_envelope_value = self.participant.vars['random_envelope_value']
+
+        # buyout offer accepted
+        if buyout <= offer:
+            self.player.payoff = c(offer)
+        # buyout offer rejected
+        else:
+            self.player.payoff = c(random_envelope_value)
+
         self.player.buyout = self.participant.vars['buyout']
         self.player.random_offer = self.participant.vars['random_offer']
         self.player.random_round = self.participant.vars['random_round']
         self.player.random_envelope_value = self.participant.vars['random_envelope_value']
         self.player.random_number = self.participant.vars['random_number']
+        self.player.offer_accepted = buyout <= offer
 
 
 page_sequence = [
     Instructions,
     FirstRoundEntry,
     RoundEntry,
-    Outcome,
+    POutcome,
+    VOutcome
 ]
