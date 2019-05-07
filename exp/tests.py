25d73236fe7a106d6ca147115b267fa5e4f2f78d
cesslab <anwarruff@gmail.com>
2019-05-07 13:32:32 -0400

    Added tests

--- exp/tests.py
+++ exp/tests.py
@@ -2,9 +2,21 @@ from otree.api import Currency as c, currency_range
 from . import pages
 from ._builtin import Bot
 from .models import Constants
+import random
 
 
 class PlayerBot(Bot):
-
     def play_round(self):
-        pass
+        if self.round_number == 1:
+            yield(pages.Instructions)
+
+        if self.round_number == 1:
+            yield(pages.FirstRoundEntry, {'buyout': random.uniform(0, 100)})
+        else:
+            yield(pages.RoundEntry, {'buyout': random.uniform(0, 100)})
+            if self.round_number == Constants.num_rounds:
+                if self.player.session.config['probability_treatment']:
+                    yield(pages.POutcome)
+                else:
+                    yield(pages.VOutcome)
+
